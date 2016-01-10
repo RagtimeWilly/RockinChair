dd004643cfb22d0c6cf6e26c978f41def1665f15
RagtimeWilly <sean.comiskey@gmail.com>
2016-01-10 21:13:30 +1100

    Added Spotify Config

---
+++ src/RockinChair/SpotifyConfig.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RockinChair
+{
+    public class SpotifyConfig
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+    
+        public string Redirect { get; set; }
+    }
+}
