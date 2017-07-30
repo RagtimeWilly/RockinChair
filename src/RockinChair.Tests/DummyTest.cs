199d4b69d3ca7e8ca6081de33fb491ba741fbcdf
RagtimeWilly <sean.comiskey@gmail.com>
2017-07-30 12:32:44 +1000

    Removed SpotifyClient as unauthenticated calls are no longer allowed
    https://developer.spotify.com/news-stories/2017/01/27/removing-unauthenticated-calls-to-the-web-api/

---
+++ src/RockinChair.Tests/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace RockinChair.Tests
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
--- src/RockinChair/SpotifyAuthorizedClient.cs
+++ src/RockinChair/SpotifyAuthorizedClient.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RockinChair.Data;
 
 namespace RockinChair
 {
-    public class SpotifyAuthorizedClient : SpotifyClient
+    public class SpotifyAuthorizedClient
     {
+        private const string BaseUrl = "https://api.spotify.com/v1";
+
+        private readonly Func<HttpClient> _clientFactory;
+
         private string _token;
 
         public SpotifyAuthorizedClient(Func<HttpClient> clientFactory, string token)
-            : base(clientFactory)
         {
+            _clientFactory = clientFactory;
             _token = token;
         }
 
@@ -20,11 +27,29 @@ namespace RockinChair
             _token = newToken;
         }
 
+        public async Task<IEnumerable<Track>> SearchTracks(string artist, string trackName)
+        {
+            var uri = new Uri($"{BaseUrl}/search?q=track:{trackName}%20artist:{artist}&type=track");
+
+            using (var httpClient = _clientFactory())
+            {
+                var header = new AuthenticationHeaderValue("Bearer", _token);
+
+                httpClient.DefaultRequestHeaders.Authorization = header;
+
+                var json = await httpClient.GetStringAsync(uri);
+
+                var tracks = JsonConvert.DeserializeObject<Tracks>(json);
+
+                return tracks.Data.Items;
+            }
+        }
+
         public async Task<string> GetPlaylists(string username)
         {
             var url = $"https://api.spotify.com/v1/users/{username}/playlists";
 
-            using (var httpClient = ClientFactory())
+            using (var httpClient = _clientFactory())
             {
                 var header = new AuthenticationHeaderValue("Bearer", _token);
 
@@ -42,7 +67,7 @@ namespace RockinChair
         {
             var url = $"https://api.spotify.com/v1/users/{username}/playlists/{playlist}/tracks?uris={track}";
 
-            using (var httpClient = ClientFactory())
+            using (var httpClient = _clientFactory())
             {
                 var header = new AuthenticationHeaderValue("Bearer", _token);
 
